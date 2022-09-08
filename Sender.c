672f916de49f9e4a9cba8ca0129a662f75b86314
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-08 15:06:04 +0530

    Update Sender.c

--- Sender.c
+++ Sender.c
@@ -2,6 +2,8 @@
 
 int sender(int fd1[]) {
     char concat_str[100];
+    char input_str[100];
+    strncpy(input_str, "www.geeks", 10);
     close(fd1[0]);
     write(fd1[1], input_str, strlen(input_str) + 1);
     close(fd1[1]);
