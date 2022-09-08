d8455c46c39935848b9db810ab6647222332c331
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-09 00:30:20 +0530

    Update SerialComm.c

--- SerialComm.c
+++ SerialComm.c
@@ -5,35 +5,39 @@ pid_t createProcess() {
 }
 
 BMSData generateSensorBMSData(BMSData bmsParam) {
-    int dataIndex = 0;
-  //  for (int dataIndex = 0; dataIndex < 2/*NUMOFREADINGS*/; dataIndex++) {
+   // int dataIndex = 0;
+    for (int dataIndex = 0; dataIndex < NUMOFREADINGS; dataIndex++) {
         bmsParam.temp[dataIndex] = (rand()%(MAX_TEMP - MIN_TEMP + 1));
         bmsParam.soc[dataIndex] = (rand()%(MAX_SOC - MIN_SOC + 1)) + MIN_SOC;
-        printf("[%d]: %d %d\n", dataIndex, bmsParam.temp[dataIndex], bmsParam.soc[dataIndex]);
-    dataIndex++;
-   // }
+        //printf("[%d]: %d %d\n", dataIndex, bmsParam.temp[dataIndex], bmsParam.soc[dataIndex]);
+   // dataIndex++;
+    }
     return bmsParam;
 }
 
 int main() {
     int fd1[2];
     pid_t p;
+    BMSData bmsParam[MAXSENSORCNT];
     //int dataLen = 0;
     if (pipe(fd1) == -1) {
         fprintf(stderr, "Pipe Failed");
     }
     p = createProcess();
-   // for (int sensorIndex = 0; sensorIndex < MAXSENSORCNT; sensorIndex++) {
-        for (int dataIndex = 0; dataIndex < NUMOFREADINGS; dataIndex++) {
+
+    for (int sensorIndex = 0; sensorIndex < 1/*MAXSENSORCNT*/; sensorIndex++) {
+        bmsParam[sensorIndex] = generateSensorBMSData(bmsParam[sensorIndex]);
+    }
+       // for (int dataIndex = 0; dataIndex < NUMOFREADINGS; dataIndex++) {
             if (p < 0) {
                 fprintf(stderr, "fork Failed");
                 break;
             } else if (p > 0) {
-                sender(fd1);
+                sender(fd1, bmsParam[0]);
             } else {
                 receiver(fd1);
             }
-        }
+      //  }
    // }
  return 0;
 }
