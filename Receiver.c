ac7c56d759b67b8315aeb7a7b0cf79341115bee4
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-08 23:30:32 +0530

    Update Receiver.c

--- Receiver.c
+++ Receiver.c
@@ -1,14 +1,14 @@
 #include "SerialCommInterface.h"
 
 void readFrom(int fd1, BMSData bmsParam, int dataLen) {
-    for (int dataIndex = 0; dataIndex < 1/*NUMOFREADINGS*/; dataIndex++) {
+   // for (int dataIndex = 0; dataIndex < 2/*NUMOFREADINGS*/; dataIndex++) {
         // read(fd1, &bmsParam.temp[dataIndex], sizeof(int));
         // read(fd1, &bmsParam.soc[dataIndex], sizeof(int));
         char tempArray[100];
         read(fd1, tempArray, sizeof(int));
         printf("%s", tempArray);
         // printf("%d %d\n", bmsParam.temp[dataIndex], bmsParam.soc[dataIndex]);
-    }
+    //}
 }
 
 void receiver(int fd1[], int dataLen) {
