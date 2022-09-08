77f97b3fac1f1609cf67687b08343377b7f6ca79
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-09 00:57:45 +0530

    Update Receiver.c

--- Receiver.c
+++ Receiver.c
@@ -1,9 +1,10 @@
 #include "SerialCommInterface.h"
 
 void readFrom(int fd1) {
-    char dataArray[500];
+    char dataArray[350];
+    memset(dataArray, '\0', sizeof(dataArray));
     read(fd1, dataArray, 350);
-    printf("receiver: %s", dataArray);
+    printf("receiver:\n %s", dataArray);
 }
 
 void receiver(int fd1[]) {
