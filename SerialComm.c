188f88badb98f6656706c10bfcd56337aa556ccf
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-08 14:35:21 +0530

    Update SerialComm.c

--- SerialComm.c
+++ SerialComm.c
@@ -8,9 +8,9 @@
 int main()
 {
     int fd1[2];
-    int fd2[2];
+    //int fd2[2];
  
-    char fixed_str[] = "forgeeks.org";
+    //char fixed_str[] = "forgeeks.org";
     char input_str[100];
     pid_t p;
  
@@ -18,10 +18,10 @@ int main()
         fprintf(stderr, "Pipe Failed");
         return 1;
     }
-    if (pipe(fd2) == -1) {
+    /*if (pipe(fd2) == -1) {
         fprintf(stderr, "Pipe Failed");
         return 1;
-    }
+    }*/
     strncpy(input_str, "www.geeks", 10);
     p = fork();
     if (p < 0) {
@@ -37,29 +37,29 @@ int main()
  
         wait(NULL);
  
-        close(fd2[1]); 
+        //close(fd2[1]); 
 
-        read(fd2[0], concat_str, 100);
-        printf("Concatenated string %s\n", concat_str);
-        close(fd2[0]);
+        //read(fd2[0], concat_str, 100);
+        //printf("Concatenated string %s\n", concat_str);
+        //close(fd2[0]);
     } else {
         close(fd1[1]);
  
         char concat_str[100];
         read(fd1[0], concat_str, 100);
+        printf("Concatenated string %s\n", concat_str);
+        //int k = strlen(concat_str);
+        //int i;
+        //for (i = 0; i < strlen(fixed_str); i++)
+          //  concat_str[k++] = fixed_str[i];
  
-        int k = strlen(concat_str);
-        int i;
-        for (i = 0; i < strlen(fixed_str); i++)
-            concat_str[k++] = fixed_str[i];
- 
-        concat_str[k] = '\0';
+        //concat_str[k] = '\0';
  
         close(fd1[0]);
-        close(fd2[0]);
+        //close(fd2[0]);
  
-        write(fd2[1], concat_str, strlen(concat_str) + 1);
-        close(fd2[1]);
+        //write(fd2[1], concat_str, strlen(concat_str) + 1);
+        //close(fd2[1]);
  
         exit(0);
     }
