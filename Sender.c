be061158fed56a7214bc191ebc6814b2342748f3
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-08 17:35:50 +0530

    Update Sender.c

--- Sender.c
+++ Sender.c
@@ -1,11 +1,24 @@
 #include "SerialCommInterface.h"
+void writeTo(BMSData bmsParam) {
+    for (int dataIndex = 0; dataIndex < NUMOFREADINGS; dataIndex++) {
+        write(fd1[1], bmsParam.temperature[dataIndex], int);
+        write(fd1[1], bmsParam.soc[dataIndex], int);
+    }
+}
 
 int sender(int fd1[]) {
-    char concat_str[100];
-    char input_str[100];
-    strncpy(input_str, "www.geeks", 10);
+    //char concat_str[100];
+    //char input_str[100];
+    //BMSData bmsParam[MAXSENSORCNT];
+    BMSData bmsParam;
+    //for (int sensorIndex = 0; sensorIndex < MAXSENSORCNT; sensorIndex++) {
+        generateSensorBMSData(bmsParam);
+    //}
+    //strncpy(input_str, "www.geeks", 10);
     close(fd1[0]);
-    write(fd1[1], input_str, strlen(input_str) + 1);
+    //for (int sensorIndex = 0; sensorIndex < MAXSENSORCNT; sensorIndex++) {
+        writeTo(bmsParam);
+    //}
     close(fd1[1]);
     return 0;
 }
