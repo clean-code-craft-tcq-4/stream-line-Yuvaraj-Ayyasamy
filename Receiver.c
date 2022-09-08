0b64bb77dfb801728e8af5d4ee1a3d0eae3eca71
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-08 23:19:14 +0530

    Update Receiver.c

--- Receiver.c
+++ Receiver.c
@@ -1,23 +1,19 @@
 #include "SerialCommInterface.h"
 
-void readFrom(int fd1, BMSData bmsParam) {
-    for (int dataIndex = 0; dataIndex < NUMOFREADINGS; dataIndex++) {
-        read(fd1, &bmsParam.temp[dataIndex], sizeof(int));
-        read(fd1, &bmsParam.soc[dataIndex], sizeof(int));
-        printf("%d %d\n", bmsParam.temp[dataIndex], bmsParam.soc[dataIndex]);
+void readFrom(int fd1, BMSData bmsParam, int dataLen) {
+    for (int dataIndex = 0; dataIndex < 1/*NUMOFREADINGS*/; dataIndex++) {
+        // read(fd1, &bmsParam.temp[dataIndex], sizeof(int));
+        // read(fd1, &bmsParam.soc[dataIndex], sizeof(int));
+        char tempArray[100];
+        read(fd1, tempArray, sizeof(int));
+        printf("%s", tempArray);
+        // printf("%d %d\n", bmsParam.temp[dataIndex], bmsParam.soc[dataIndex]);
     }
 }
 
-int receiver(int fd1[]) {
+void receiver(int fd1[], int dataLen) {
     close(fd1[1]);
-    //BMSData bmsParam[MAXSENSORCNT];
     BMSData bmsParam;
-    //char concat_str[100];
-    //read(fd1[0], concat_str, 100);
-    //printf("Concatenated string %s\n", concat_str);
-    //for (int sensorIndex = 0; sensorIndex < MAXSENSORCNT; sensorIndex++) {
-        readFrom(fd1[0], bmsParam);
-    //}
+    readFrom(fd1[0], bmsParam, dataLen);
     close(fd1[0]);
-    return 0;
 }
