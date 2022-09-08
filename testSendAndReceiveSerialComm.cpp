372f6142d610f4cf720eca999e3a2bb75c3c21a8
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-09 02:29:26 +0530

    Create testSendAndReceiveSerialComm.cpp

---
+++ testSendAndReceiveSerialComm.cpp
@@ -0,0 +1,40 @@
+#include "SerialCommInterface.h"
+
+void testgenerateSensorBMSData()
+{
+    BMSData bmsParam;
+    int index=0;
+    for (; index<NUMOFREADINGS; index++) {
+        bmsParam.temp[index] = 0;
+        bmsParam.soc[index] = 0;
+    }
+    bmsParam = generateSensorBMSData(bmsParam);
+    for (index=0; index<NUMOFREADINGS; index++) {
+        assert((bmsParam.temp[index] != 0) && (bmsParam.soc[index] != 0));
+    }
+}
+
+void testcreateProcess()
+{
+    assert(createProcess() >= SUCCESS);
+}
+
+void testpipeInit()
+{
+    int fd1[2];
+    assert(pipeInit(fd1) == SUCCESS);
+}
+
+void testcommInit()
+{
+    assert(commInit() == SUCCESS);
+}
+
+int main()
+{
+    testgenerateSensorBMSData();
+    testcreateProcess();
+    testpipeInit();
+    testcommInit();
+    return 0;
+}
