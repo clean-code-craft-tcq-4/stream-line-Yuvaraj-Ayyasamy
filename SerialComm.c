1265c2e31186bcbe7c20157fa9f1f361fba3bc3f
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-09 01:41:22 +0530

    Update SerialComm.c

--- SerialComm.c
+++ SerialComm.c
@@ -23,20 +23,17 @@ int main() {
         fprintf(stderr, "Pipe Failed");
     }
 
-    //for (int sensorIndex = 0; sensorIndex < MAXSENSORCNT; sensorIndex++) {
     p = createProcess();
     if (p < 0) {
         fprintf(stderr, "fork Failed");
     } else if (p > 0) {
         for (int sensorIndex = 0; sensorIndex < MAXSENSORCNT; sensorIndex++) {
             bmsParam[sensorIndex] = generateSensorBMSData(bmsParam[sensorIndex]);
-            //sender(fd1, bmsParam[sensorIndex]);
         }
         sender(fd1, bmsParam);
     } else {
         receiver(fd1);
     }
-   // exit(0);
-    //}
+
  return 0;
 }
