f2ffd3f61a8d24ec205f1db5e3d7f75515ba06ce
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-09 00:48:19 +0530

    Update Sender.c

--- Sender.c
+++ Sender.c
@@ -1,28 +1,18 @@
 #include "SerialCommInterface.h"
 
 void writeTo(int fd1, BMSData bmsParam) {
- //   static int dataIndex = 0;
     char dataArray[1000];
     for (int dataIndex = 0; dataIndex < NUMOFREADINGS; dataIndex++) {
         char tempArray[10];
         sprintf(tempArray, "%d %d\n", bmsParam.temp[dataIndex], bmsParam.soc[dataIndex]);
-        //dataLen += strlen(tempArray);
-       // write(fd1, dataArray, strlen(dataArray));
-        //printf("sender:[%d] %s\n", dataIndex, dataArray);
         strcat(dataArray, tempArray);
-       // write(fd1, &bmsParam.temp[dataIndex], sizeof(int));
-       // write(fd1, &bmsParam.soc[dataIndex], sizeof(int));
     }
     printf("sender: %s\n", dataArray);
     write(fd1, dataArray, strlen(dataArray));
 }
 
 void sender(int fd1[], BMSData bmsParam) {
-    //BMSData bmsParam;
-    //int dataLen;
-    //bmsParam = generateSensorBMSData(bmsParam);
     close(fd1[0]);
     writeTo(fd1[1], bmsParam);
     close(fd1[1]);
-    //return dataLen;
 }
