eb6a235c077dbdf49d42d19b9e9009eecae42734
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-09-08 17:41:16 +0530

    Update Receiver.c

--- Receiver.c
+++ Receiver.c
@@ -1,10 +1,10 @@
 #include "SerialCommInterface.h"
 
-void readFrom(BMSData bmsParam) {
+void readFrom(int fd1, BMSData bmsParam) {
     for (int dataIndex = 0; dataIndex < NUMOFREADINGS; dataIndex++) {
-        read(fd1[1], bmsParam.temperature[dataIndex], int);
-        read(fd1[1], bmsParam.soc[dataIndex], int);
-        printf("%d %d\n", bmsParam.temperature[dataIndex], bmsParam.soc[dataIndex]);
+        read(fd1, bmsParam.temp[dataIndex], int);
+        read(fd1, bmsParam.soc[dataIndex], int);
+        printf("%d %d\n", bmsParam.temp[dataIndex], bmsParam.soc[dataIndex]);
     }
 }
 
@@ -16,7 +16,7 @@ int receiver(int fd1[]) {
     //read(fd1[0], concat_str, 100);
     //printf("Concatenated string %s\n", concat_str);
     //for (int sensorIndex = 0; sensorIndex < MAXSENSORCNT; sensorIndex++) {
-        readFrom(bmsParam);
+        readFrom(fd1[0], bmsParam);
     //}
     close(fd1[0]);
     return 0;
